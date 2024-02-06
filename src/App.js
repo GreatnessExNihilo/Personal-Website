2b0850394136873ac1559f73588eb1f33a5a021b
Omar <omarraymond18@gmail.com>
2024-02-05 23:21:27 -0500

    Added folder for website pages. Added Homepage and links to different pages in Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './website-pages/HomePage';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Navbar/>
+          <Routes>
+            <Route path="/" element={<HomePage />}/>
+          </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import AndroidBotLogo from '../assets/androidbotlogo.png'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div classname="navbar">
         <div classname="leftSideOfNavBar"></div>
             <img src={AndroidBotLogo} />
-        <div classname="rightSideOfNavBar"></div>
+        <div classname="rightSideOfNavBar">
+            <Link to="/"> Home </Link>
+            <Link to="/resume"> Resume </Link>
+            <Link to="/aboutme"> About Me </Link>
+            <Link to="/projects"> Projects </Link>
+        </div>
     </div>
   );
 }
